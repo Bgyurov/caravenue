482559a96eba75455d10b2900e96674bdf59e1ec
Bgyurov <49366221+Bgyurov@users.noreply.github.com>
2023-12-04 15:37:01 +0200

    Add seedData and update fonts

--- carAvenue vite/client/src/components/NotFound/NotFound.jsx
+++ carAvenue vite/client/src/components/NotFound/NotFound.jsx
@@ -5,14 +5,15 @@ const NotFound = () => {
     const theme = createTheme({
         palette: {
           ochre: {
-            main: '#494545',
-        
-            contrastText: '#fff',
+            main: '#E3D026',
+            light: '#E9DB5D',
+            dark: '#A29415',
+            contrastText: '#242105',
           },
         },
       });
     return (
-        <Container maxWidth="md" sx={{ color: "white", textAlign: 'center', mt: 4 }}>
+        <Container maxWidth="md" sx={{ color: "white", textAlign: 'center', mt: 4,  }}>
           <Typography variant="h1" component="div" gutterBottom>
             404
           </Typography>
@@ -20,7 +21,7 @@ const NotFound = () => {
             Oops! The page you are looking for might be unavailable.
           </Typography>
           <ThemeProvider theme={theme}>
-          <Button component={Link} to="/" variant="contained" color="ochre">
+          <Button sx={{marginBottom: '50px'}} component={Link} to="/" variant="contained" color="ochre">
             Go to Home
           </Button>
           </ThemeProvider>
